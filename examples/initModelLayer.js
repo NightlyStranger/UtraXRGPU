9dbe5a13758a0b3da277e77eaef8c52907a18429
Maxim Perepelitsa <school23.max@gmail.com>
2025-12-18 09:05:27 +0100

    Update initModelLayer.js

--- examples/initModelLayer.js
+++ examples/initModelLayer.js
@@ -663,7 +663,7 @@ export function initModelLayer(
         async function loadAllVolumes() {
             const volumePromises = [];
 
-            for (let i = 1; i <= 1; i++) {
+            for (let i = 1; i <= 4; i++) {
                 const volumePath = `volumes/Frame${String(i).padStart(2,'0')}/Volume.downsampled.raw`;
                 console.log(volumePath);
                 volumePromises.push(loadVolume(volumePath));
